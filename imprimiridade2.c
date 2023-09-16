8b3f2850a28361d8f68221e9949bcc3c094e2080
Thais Leite <145159951+thaissleiite@users.noreply.github.com>
2023-09-16 11:03:15 -0300

    Add files via upload

---
+++ imprimiridade2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+     int idade, idade_nova;
+
+     printf("insira a sua idade:\n");
+     scanf("%d", &idade);
+
+     idade_nova = idade + 1;
+
+     printf("no ano que vem voce tera %d anos", idade_nova);
+     }
